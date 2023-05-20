8f1091ed92a91503095ef5738184505a8e4140ea
MANOJ GUJJALA <balu88861@gmail.com>
2023-05-20 23:49:58 +0530

    secondCommit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 
-import axios from 'axios';
+
 import './App.css';
 
 import api from './api/axiosConfig';
 import { useState,useEffect } from 'react';
+import Layout from './components/Layout';
+import { Routes,Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+
+
 
 function App() {
   const [movies,setMovies]=useState();
-
+  console.log(movies);
+  
   const getMovies=async()=>{
 
     try
     {
       let url="https://378f-2405-201-c049-7091-2cdf-6ec7-b15b-9e45.ngrok-free.app/api/v1/movies"
-      
+      console.log("--------------");
       const response = await api.get(url);
-      console.log(response.data);
       setMovies(response.data);
 
     } 
@@ -29,7 +34,13 @@ function App() {
   },[])
   return (
     <div className="App">
-      <h1>Hai hello</h1>
+     
+      <Routes>
+        <Route path='/' element={<Layout/>}>
+
+          <Route path='/' element={<Home movies={movies}/>}></Route>
+        </Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+
+import Hero from "../Hero/Hero"
+
+const Home=({movies})=>{
+    
+    return(
+       <Hero movies={movies}/>
+    )
+
+}
+
+export default Home
